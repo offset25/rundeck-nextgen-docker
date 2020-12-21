41abb21d78a4b846d7af475247e0d72c0d0b42fd
Dino Dev <dinodev>
2020-12-21 02:56:40 +0000

    added support for admin password

--- init-rundeck-docker.sh
+++ init-rundeck-docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-read -p "Enter external server URL without the port (e.g. http://rundeck.lan): " external_server_url
+read -p "Enter external server URL without the port (default is set to http://rundeck.lan): " external_server_url
 read -p "Enter external server port (default is set to 80): " external_server_port
+read -p "Enter admin password (default is set to: \"rundeck\"): " rundeck_admin_password
 
 
 # if apt installed
@@ -30,7 +31,7 @@ if test -z "$external_server_url"
 then
       echo ""
 else
-	echo " is NOT empty"
+	#echo " is NOT empty"
 	old='EXTERNAL_SERVER_URL=http://rundeck.lan'
 	new="EXTERNAL_SERVER_URL=$external_server_url"
 
@@ -41,9 +42,20 @@ if test -z "$external_server_port"
 then
       echo ""
 else
-	echo " is NOT empty"
+	#echo " is NOT empty"
 	old='EXTERNAL_SERVER_PORT=80'
 	new="EXTERNAL_SERVER_PORT=$external_server_port"
+
+	sed -i -e 's,'"$old"','"$new"',g' docker-image-rundeck/.env
+fi
+
+if test -z "$rundeck_admin_password" 
+then
+      echo ""
+else
+	#echo " is NOT empty"
+	old='RUNDECK_ADMIN_PASSWORD=rundeck'
+	new="RUNDECK_ADMIN_PASSWORD=$rundeck_admin_password"
 
 	sed -i -e 's,'"$old"','"$new"',g' docker-image-rundeck/.env
 fi
